522bd48a2ab7a86bd929c09f371212af4aa4febd
SubhamMohanta <subhammohanta99@gmail.com>
2024-04-29 13:01:44 +0530

    Done

---
+++ forms/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Forms from './components/Forms'
+import './App.css'
+
+export default function App() {
+  return (
+    <div id='app'>
+      <Forms/>
+    </div>
+  )
+}
